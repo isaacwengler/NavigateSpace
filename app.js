e3b43aebe02331a9a7766f2308d0f7abbb62275a
Iernst95 <Isaacernst95@gmail.com>
2021-11-06 10:55:54 -0500

    solid planets have surface view

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//import * as ground from '/views/groundView.js'
+import { showGroundView } from '/views/groundView.js'
 import { showPlanetView } from '/views/planetView.js'
 import { slipspace } from "./views/slipspace.js";
 //import * as solar from '/views/solarsystem.js'
 
 //showPlanetView('planet');
-slipspace();
+showGroundView('earth');
 
