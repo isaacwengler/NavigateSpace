fc20a6c7d3f86a35b8b266b822db04b6f02cad52
EthanLLoftis <ethan.l.loftis@gmail.com>
2021-11-06 17:04:45 -0500

    planets orgbiting properly

--- views/solarsystem.js
+++ views/solarsystem.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/contro
 
 export function solarView(){
 
-
+    const date = Date.now() * 0.0001;
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 2000);
     const renderer = new THREE.WebGLRenderer({
@@ -24,11 +24,12 @@ export function solarView(){
     const sun = new THREE.Mesh(sunSphere,sunMaterial);
 
 
-    const mercShpere = new THREE.SphereGeometry(6,15,15);
-    mercShpere.translate(100,0,0)
+    const mercSphere = new THREE.SphereGeometry(6,15,15);
+    mercSphere.translate(100,0,0);
+    //mercSphere.translate(Math.cos(date) * 100,0, Math.sin(date) * 100);
     const mercTexture = new THREE.TextureLoader().load("./Images/mercuryUV.jpg")
     const mercMaterial = new THREE.MeshBasicMaterial({map: mercTexture})
-    const mercury = new THREE.Mesh(mercShpere,mercMaterial);
+    const mercury = new THREE.Mesh(mercSphere,mercMaterial);
 
 
     const venusSphere = new THREE.SphereGeometry(14,15,15);
@@ -90,9 +91,79 @@ export function solarView(){
     venusRing.rotation.set(1.57079575,0,0);
     scene.add(venusRing);
 
-    
+    const earthRingGeometry = new THREE.TorusGeometry(195,0.5,2.5,100);
+    const earthRing =  new THREE.Mesh(earthRingGeometry,earthMaterial);
+    earthRing.rotation.set(1.57079575,0,0);
+    scene.add(earthRing);
 
-    scene.add(sun,mercury,venus,earth,mars,jupiter,saturn,uranus,neptune,pluto);
+    const marsRingGeometry = new THREE.TorusGeometry(230,0.5,2.5,100);
+    const marsRing = new THREE.Mesh(marsRingGeometry,marsMaterial);
+    marsRing.rotation.set(1.57079575,0,0);
+    scene.add(marsRing);
+
+    const jupRingGeometry = new THREE.TorusGeometry(300,0.5,2.5,100);
+    const jupRing = new THREE.Mesh(jupRingGeometry,jupMaterial);
+    jupRing.rotation.set(1.57079575,0,0);
+    scene.add(jupRing);
+
+    const satRingGeometry = new THREE.TorusGeometry(390,0.5,2.5,100);
+    const satRing = new THREE.Mesh(satRingGeometry,satMaterial);
+    satRing.rotation.set(1.57079575,0,0);
+    scene.add(satRing);
+
+    const uranRingGeometry = new THREE.TorusGeometry(460,0.5,2.5,100);
+    const uranRing = new THREE.Mesh(uranRingGeometry,uranMaterial);
+    uranRing.rotation.set(1.57079575,0,0);
+    scene.add(uranRing);
+
+    const nepRingGeometry = new THREE.TorusGeometry(520,0.5,2.5,100);
+    const nepRing = new THREE.Mesh(nepRingGeometry,nepMaterial);
+    nepRing.rotation.set(1.57079575,0,0);
+    scene.add(nepRing);
+
+    const plutoRingGeometry = new THREE.TorusGeometry(570,0.5,2.5,100);
+    const plutoRing = new THREE.Mesh(plutoRingGeometry,plutoMaterial);
+    plutoRing.rotation.set(1.57079575,0,0);
+    scene.add(plutoRing);
+
+    //orbit animations
+    const mercOrbit = new THREE.Group();
+    mercOrbit.add(mercury);
+    mercOrbit.add(mercRing);
+
+    const venusOrbit = new THREE.Group();
+    venusOrbit.add(venus);
+    venusOrbit.add(venusRing);
+    
+    const earthOrbit = new THREE.Group();
+    earthOrbit.add(earth);
+    earthOrbit.add(earthRing);
+
+    const marsOrbit = new THREE.Group();
+    marsOrbit.add(mars);
+    marsOrbit.add(marsRing);
+
+    const jupOrbit = new THREE.Group();
+    jupOrbit.add(jupiter);
+    jupOrbit.add(jupRing);
+
+    const satOrbit = new THREE.Group();
+    satOrbit.add(saturn);
+    satOrbit.add(satRing);
+
+    const uranOrbit = new THREE.Group();
+    uranOrbit.add(uranus);
+    uranOrbit.add(uranRing);
+
+    const nepOrbit = new THREE.Group();
+    nepOrbit.add(neptune);
+    nepOrbit.add(nepRing);
+
+    const plutoOrbit = new THREE.Group();
+    plutoOrbit.add(pluto);
+    plutoOrbit.add(plutoRing);
+
+    scene.add(sun,mercOrbit,venusOrbit,earthOrbit,marsOrbit,jupOrbit,satOrbit,uranOrbit,nepOrbit,plutoOrbit);
 
     camera.position.set(350,800,400);
     controls.update();
@@ -100,13 +171,17 @@ export function solarView(){
     function animate() {
         requestAnimationFrame(animate);
         controls.update();
+        mercOrbit.rotation.y += 0.0479;
+        venusOrbit.rotation.y += 0.0350;
+        earthOrbit.rotation.y += 0.0298;
+        marsOrbit.rotation.y += 0.0241;
+        jupOrbit.rotation.y += 0.0131;
+        satOrbit.rotation.y += 0.0097;
+        uranOrbit.rotation.y += 0.0068;
+        nepOrbit.rotation.y += 0.0054;
+        plutoOrbit.rotation.y += 0.01;
         renderer.render(scene,camera);
     }
-
-
-    // function createOrbits() {
-    //     var orbitWidth = 0.01;
-    // }
 
     animate();
 }
