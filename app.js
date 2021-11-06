bb2eab57f3445d3ec834b577330e544bc38c012d
Kehinde Fatoki <fatokik@gmail.com>
2021-11-06 16:12:12 -0500

    fixed path

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-import { showGroundView } from "/views/groundView.js";
-import { showPlanetView, changeCamera, goToPlanet } from "/views/planetView.js";
+import { showGroundView } from "./views/groundView.js";
+import {
+  showPlanetView,
+  changeCamera,
+  goToPlanet,
+} from "./views/planetView.js";
 import { slipspace } from "./views/slipspace.js";
 //import * as solar from '/views/solarsystem.js'
 
@@ -16,47 +20,45 @@ function buttonAction(buttonSelected) {
   button.setAttribute("class", "btn btn-light");
 }
 
-
-let currentPlanet = 'earth';
+let currentPlanet = "earth";
 let animating = false;
 window.onload = showPlanetView(currentPlanet, true);
 
 const changePlanet = (planet) => {
-    if (currentPlanet === planet || animating) return;
-    currentPlanet = planet;
-    animating = true;
-    for (let i = 0; i < 40; i++) {
-        setTimeout(() => changeCamera(), 20 * i);
-    }
-    setTimeout(() => slipspace(), 800);
-    console.log(planet)
-    setTimeout(() => {
-        showPlanetView(planet, false);
-        animating = false;
-    }, 5000);
-    
-}
+  if (currentPlanet === planet || animating) return;
+  currentPlanet = planet;
+  animating = true;
+  for (let i = 0; i < 40; i++) {
+    setTimeout(() => changeCamera(), 20 * i);
+  }
+  setTimeout(() => slipspace(), 800);
+  console.log(planet);
+  setTimeout(() => {
+    showPlanetView(planet, false);
+    animating = false;
+  }, 5000);
+};
 window.changePlanet = changePlanet;
 
 const visitPlanet = () => {
-    const menu = document.getElementById('planetControls');
-    menu.hidden = true;
-    for (let i = 0; i < 20; i++) {
-        setTimeout(() => goToPlanet(), 40 * i);
-    };
-    setTimeout(() => {
-        const menu2 = document.getElementById('planetControls2');
-        menu2.hidden = false;
-        showGroundView(currentPlanet);
-    }, 800);
-}
+  const menu = document.getElementById("planetControls");
+  menu.hidden = true;
+  for (let i = 0; i < 20; i++) {
+    setTimeout(() => goToPlanet(), 40 * i);
+  }
+  setTimeout(() => {
+    const menu2 = document.getElementById("planetControls2");
+    menu2.hidden = false;
+    showGroundView(currentPlanet);
+  }, 800);
+};
 window.visitPlanet = visitPlanet;
 
 const backToOrbit = () => {
-    const menu = document.getElementById('planetControls');
-    menu.hidden = false;
-    const menu2 = document.getElementById('planetControls2');
-    menu2.hidden = true;
-    showPlanetView(currentPlanet, true);
-}
+  const menu = document.getElementById("planetControls");
+  menu.hidden = false;
+  const menu2 = document.getElementById("planetControls2");
+  menu2.hidden = true;
+  showPlanetView(currentPlanet, true);
+};
 window.backToOrbit = backToOrbit;
