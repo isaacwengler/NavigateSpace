d2674d2109fc9da6c978159ea36ed969634bb022
EthanLLoftis <ethan.l.loftis@gmail.com>
2021-11-05 20:23:31 -0500

    initail code for solar system view

--- views/solarsystem.js
+++ views/solarsystem.js
@@ -1,1 +1,29 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
+import './solarsystem.css';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.webGLRenderer({
+    canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render(scene,cam);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true} );
+const torus = new THREE.Mesh(geometry,material);
+
+scene.add(torus);
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+
+animate();
