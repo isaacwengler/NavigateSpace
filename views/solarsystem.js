eb5a9f3f591161ead1d1ca292bb1f8ecc042ecfa
EthanLLoftis <ethan.l.loftis@gmail.com>
2021-11-06 09:16:56 -0500

    new mercury planet

--- views/solarsystem.js
+++ views/solarsystem.js
@@ -1,6 +1,8 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
 import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js';
 
+
+//initialization
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -9,12 +11,19 @@ const controls = new OrbitControls( camera, renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(15,15,15);
-const texture = new THREE.TextureLoader().load("./views/sunUV.jpg");
-const material = new THREE.MeshBasicMaterial( {map: texture} );
-const sphere = new THREE.Mesh(geometry,material);
+//spheres
+const sunSphere = new THREE.SphereGeometry(15,15,15);
+const sunTexture = new THREE.TextureLoader().load("./views/sunUV.jpg");
+const sunMaterial = new THREE.MeshBasicMaterial( {map: sunTexture} );
+const sun = new THREE.Mesh(sunSphere,sunMaterial);
 
-scene.add(sphere);
+const mercShpere = new THREE.SphereGeometry(15,15,15);
+mercShpere.translate(35,0,0)
+const mercTexture = new THREE.TextureLoader().load("./views/mercuryUV.jpg")
+const mercMaterial = new THREE.MeshBasicMaterial({map: mercTexture})
+const mercury = new THREE.Mesh(mercShpere,mercMaterial);
+
+scene.add(sun,mercury);
 
 camera.position.set(10,40,200);
 controls.update();
