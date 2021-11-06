99cc17d4d776f59d96ff7c1696caf8e7c5acbe76
EthanLLoftis <ethan.l.loftis@gmail.com>
2021-11-05 21:11:33 -0500

    still setup

--- views/solarsystem.js
+++ views/solarsystem.js
@@ -1,19 +1,20 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
-import './solarsystem.css';
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.webGLRenderer({
-    canvas: document.querySelector('#bg')
-});
+// const renderer = new THREE.webGLRenderer({
+//     canvas: document.querySelector('#bg')
+// });
+
+const renderer = new THREE.webGLRenderer();
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
-renderer.render(scene,cam);
+renderer.render(scene,camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true} );
