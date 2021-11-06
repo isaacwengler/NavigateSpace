a148ff082a2bd38461ff42eb34667f0e8f108a20
Isaac Wengler <iwengler3@gmail.com>
2021-11-05 20:02:31 -0500

    link

--- views/groundView.js
+++ views/groundView.js
@@ -1,1 +1,1 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.skypack.dev/three';
--- views/planetView.js
+++ views/planetView.js
@@ -1,1 +1,29 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.skypack.dev/three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.
+    PerspectiveCamera(
+        75,
+        innerWidth / innerHeight,
+        0.1,
+        1000
+    );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight);
+document.body.innerHTML = ""
+document.body.appendChild(renderer.domElement);
+
+// sphere
+const sphere = new THREE
+    .Mesh(new THREE.SphereGeometry(5, 50, 50),
+    new THREE.MeshBasicMaterial({
+        color: 0xFF0000
+    }))
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
