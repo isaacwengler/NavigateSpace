8a9b1e6bc589bbcb1fc891d48e727942fa88597e
Kehinde Fatoki <fatokik@gmail.com>
2021-11-06 10:59:07 -0500

    skeleton code for controls

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //import * as ground from '/views/groundView.js'
-import { showPlanetView } from "/views/planetView.js";
+// import { showPlanetView } from "/views/planetView.js";
 import { slipspace } from "./views/slipspace.js";
 //import * as solar from '/views/solarsystem.js'
 
 //showPlanetView('planet');
-slipspace();
+// slipspace();
